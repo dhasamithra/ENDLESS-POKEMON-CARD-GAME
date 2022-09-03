3722dfa46c05e6ce0c9476427e5d3889de40f63a
O123 <74002230+dhasamithra@users.noreply.github.com>
2022-09-03 09:44:40 +0530

    Add files via upload

---
+++ ENDLESS POKEMON CARD GAME.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+player1_score = player1_score + random_power_list
+player2_score = player2_score + random_power_list2
+
+pokemon_list = [pikachu]
+power_list = [200]
+label["image"]=random_poemon
+player_1_btn = Button(root, image = img, command = player1)
+player_2_btn = Button(root, image = img, command = player2)
